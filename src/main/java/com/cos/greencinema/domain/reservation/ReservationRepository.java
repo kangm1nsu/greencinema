9d29b8d47038e2f45475d9393437c51a9a480b85
unknown <su3592@naver.com>
2021-11-01 14:59:33 +0900

    [강민수] 도메인

---
+++ src/main/java/com/cos/greencinema/domain/reservation/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.cos.greencinema.domain.reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer>{
+
+}
