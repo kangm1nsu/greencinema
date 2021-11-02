f39e3f76160ce42259ee7c577db8ed76f977ac4e
Minhong <lmh4861@gamil.com>
2021-11-02 10:14:41 +0900

    [민홍] domain 뼈대 완료 (repository는 query 문 등은 보류)

--- src/main/java/com/cos/greencinema/domain/admin/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cos.greencinema.domain.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-}
---
+++ src/main/java/com/cos/greencinema/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cos.greencinema.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	@Query(value = "select * from user where username = :username and password = :password", nativeQuery = true)
+	User mLogin(String username, String password);
+}
